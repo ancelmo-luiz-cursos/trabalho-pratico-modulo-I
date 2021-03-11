416349daa8225c4fb05f13f8e05d20059966d9d1
Ancelmo Luiz <ancelmo.luiz@hotmail.com>
2021-03-10 23:41:44 -0400

    inicio correcao

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,3 @@
-window.addEventListener('load', carregarPagina);
-
-function carregarPagina() {
-  var numeros = document.querySelector('#numeros');
-  var numero = document.querySelector('#numero');
-
-  numero.value = numeros.value;
+function start() {
+  
 }
